5dcc4cbb3954582eb1b246ca190de0a5d9e31ad8
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-04 14:51:02 -0700

    Add files via upload

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Acounting Systems \nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
