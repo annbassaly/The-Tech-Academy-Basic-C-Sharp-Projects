47b321b5df57b666aa84d91c3d4dcd5a4f600cb4
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-11 11:12:06 -0700

    Add files via upload
    Assignment step 202

---
+++ IterationAssignment1/IterationAssignment1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string[] names = { "Mark", "Donna", "Bill", "Jane" };
+
+        Console.WriteLine("What is your name?");
+        string yourName = Console.ReadLine();
+  
+        foreach (string items in names)
+        {
+            Console.WriteLine(items + yourName);
+        }
+        Console.ReadLine();
+
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    Console.WriteLine("\n");
+        //    names[j] += yourName;
+        //    Console.WriteLine(names[j]);
+        //    Console.ReadLine();
+        //}
+        }
+    }
+
