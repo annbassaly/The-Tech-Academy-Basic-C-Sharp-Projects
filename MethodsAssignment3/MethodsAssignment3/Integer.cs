6f3670ad9a3a0d381920326123e119e9aa04a850
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-16 07:24:05 -0700

    C# assignment page 220

---
+++ MethodsAssignment3/MethodsAssignment3/Integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment3
+{
+    public class Integer
+    {
+        public static int Sum(int num1, int num2)
+        {
+            int total;
+            total = num1 + num2;
+            return total;
+
+        }
+    }
+
+
+    //public int Sum(int num1, int num2)
+    //{
+    //    int total2;
+    //    total2 = num1 - num2;
+    //    return total2;
+    //}
+}
