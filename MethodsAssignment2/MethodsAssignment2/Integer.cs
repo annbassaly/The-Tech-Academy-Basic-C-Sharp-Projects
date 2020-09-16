ea5e9aabda998178908b622c17722a5766dfeaf0
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-16 06:58:25 -0700

    C# assignment page 219

---
+++ MethodsAssignment2/MethodsAssignment2/Integer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment2
+{
+    public class Integer
+    //{
+    //   public static int Sum(int num1)
+    //    {
+    //        int total;
+    //        total = num1 + 5;
+    //        return total;
+    //     }
+    //}
+    //{ 
+    //   public static int Subtract(int num2)
+    //{
+    //    int total;
+    //    total = num2 - 5;
+    //    return total;
+    //}
+
+    {
+        public static int Multiply(int num2)
+        {
+            int total;
+            total = num2 * 5;
+            return total;
+        }
+    }
+}
