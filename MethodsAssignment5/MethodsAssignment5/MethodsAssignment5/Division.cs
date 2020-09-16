dfe8da48ff7e9a2827e88ebe9f3da67bff70831c
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-16 16:23:54 -0700

    c# assignment page 224

---
+++ MethodsAssignment5/MethodsAssignment5/MethodsAssignment5/Division.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment5
+{
+    class Division
+    {
+     public void Divide( int num1, out int total)
+        {
+            total = num1 / 2;
+            
+        }
+        public void Divide(int num1, out int total, int num2)
+        {
+            total = num1 / 2 + num2;
+        }
+
+    }
+}
+
---
+++ MethodsAssignment5/MethodsAssignment5/MethodsAssignment5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Division division = new Division();
+            Console.WriteLine("Enter a number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            division.Divide(n1, out int total);
+            Console.WriteLine("The difference of the two numbers is: {0}", total);
+            Console.ReadLine();
+        }
+    }
+}
