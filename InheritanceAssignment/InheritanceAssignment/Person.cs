f01bb066766154b2711f8c8fac73720b7800a04f
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-17 09:01:38 -0700

    C# Assignment page 229

---
+++ InheritanceAssignment/InheritanceAssignment/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    public class Person
+    {
+        public List<string> Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            foreach (string name in Name)
+            {
+                Console.WriteLine(name);
+            }
+            
+        }
+    }
+}
---
+++ InheritanceAssignment/InheritanceAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+            person.Name = new List<string>()
+             {
+                "Rick",
+                "Jane",
+                "Linda",
+                "Ryan"
+             };
+
+            person.SayName();
+            Console.ReadLine();
+        }
+       
+     }
+    
+}
