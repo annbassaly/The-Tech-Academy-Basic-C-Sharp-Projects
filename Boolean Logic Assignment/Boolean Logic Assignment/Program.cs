f785112b1ff96251337ae2bc4d203b31f064c383
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-17 12:50:30 -0700

    Update to c# assignment page 161

---
+++ Boolean Logic Assignment/Boolean Logic Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age? ");
+            int currentAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI (true or false)? ");
+            bool hasDui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have? ");
+            int speedTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool isQualified = (currentAge > 15 && hasDui==false && speedTickets <= 3);
+            Console.WriteLine("Qualified? " + isQualified);
+            Console.ReadLine();
+        }
+    }
+}
