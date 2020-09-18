3ecca75bf4b0b1bd154a397366fa3f4fe22076a6
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-18 12:04:27 -0700

    C# assignment page 197

---
+++ IterationStep197/IterationStep197/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Mark\n", "Donna\n", "Bill\n", "Jane\n" };
+
+        Console.WriteLine("What is your name?");
+        string yourName = Console.ReadLine();
+
+         for (int j = 0; j < names.Length; j++)
+        {
+            Console.WriteLine("\n");
+            names[j] += yourName;
+            Console.WriteLine(names[j]);
+            Console.ReadLine();
+        }
+    }
+}
