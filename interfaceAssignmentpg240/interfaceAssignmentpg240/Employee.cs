7ffaa825bcb13d458a5f50bfd29a2a673ea1e9ba
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-22 16:45:34 -0700

    Interface assignment c# page 240

---
+++ interfaceAssignmentpg240/interfaceAssignmentpg240/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace interfaceAssignmentpg240
+{
+    public class Employee : Person, IQuittable
+    {
+        public override string SayName()
+        {
+            return base.SayName();
+        }
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine(employee.SayName() + " " + "has quit");
+        }
+    }
+}
---
+++ interfaceAssignmentpg240/interfaceAssignmentpg240/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace interfaceAssignmentpg240
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+            {
+                firstName = "Sample",
+                lastName = "Student",
+            };
+            obj.SayName();
+            IQuittable quittable = new Employee();
+            quittable.Quit(obj);
+            Console.ReadLine();
+
+        }
+
+    }
+}
