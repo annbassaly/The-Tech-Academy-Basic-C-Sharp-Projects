034420d379190e8dc181571e29f5251bd662a624
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-21 17:14:29 -0700

    C# assignment pg 237

---
+++ abstractAssignmentpg237/abstractAssignmentpg237/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractAssignmentpg237
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName();
+
+        //public virtual void ListPlayers()
+        //{
+        //    foreach (string player in Players)
+        //    {
+        //        Console.WriteLine(player);
+        //    }
+
+        //}
+    }
+}
+
---
+++ abstractAssignmentpg237/abstractAssignmentpg237/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractAssignmentpg237
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.firstName = "Sample ";
+            person.lastName = "Student";
+            person.SayName();    
+            Console.WriteLine(person.firstName + person.lastName);
+            Console.ReadLine();
+        }
+    }
+}
