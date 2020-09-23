aaa3c41c222984d13c3955359c91ce6a2b058ed4
annbassaly <68878534+annbassaly@users.noreply.github.com>
2020-09-22 17:37:55 -0700

    Add files via upload

---
+++ interfaceAssignmentpg240/interfaceAssignmentpg240/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace interfaceAssignmentpg240
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            {
+                person.firstName = "Sample";
+                person.lastName = "Student";
+            };
+            person.SayName();
+            IQuittable quittable = new Employee();
+            quittable.Quit(person);
+            Console.ReadLine();
+
+        }
+
+    }
+}
